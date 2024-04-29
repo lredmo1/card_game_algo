bc48aa29c07f253f12ec65fb5e0d01abdf326e90
Lisa Primeaux-Redmond <redlisa@gmail.com.com>
2024-04-28 15:04:50 -1000

    refactor

--- war_algo.js
+++ war_algo.js
@@ -20,9 +20,7 @@ const war = (arr1, arr2, breakOut) => {
 		// If Player 1's card is greater than Player 2's card, add both cards to the bottom of Player 1's deck, remove from the top of both players decks, and continue the game from the top	
 		} else if (arr1[0] > arr2[0]) {
 		
-			arr1.push(arr1[0], arr2[0])
-			arr1.splice(0, 1)
-			arr2.splice(0, 1)
+			arr1.push(arr1.splice(0, 1)[0], arr2.splice(0, 1)[0])
 
 			// If a true value has been passed into the recursive use of the function, exit the loop because Player 1 has won  the war
 			if (breakOut) return 1 
@@ -30,9 +28,7 @@ const war = (arr1, arr2, breakOut) => {
 		// If Player 2's card is greater than Player 1's card, add both cards to the bottom of Player 2's deck, remove from the top of both players decks, and continue the game from the top	
 		} else if (arr1[0] < arr2[0]) {
 		
-			arr2.push(arr2[0], arr1[0])
-			arr1.splice(0, 1)
-			arr2.splice(0, 1)
+			arr2.push(arr2.splice(0, 1)[0], arr1.splice(0, 1)[0])
 			
 			// If a true value has been passed into the recursive use of the function, exit the loop because Player 1 has won the war
 			if (breakOut) return 2
